4cd6365b4f510cc53f2f332baf698bef1185e978
Jeimy Marcela Mateus Ariza <jeimymateus1@gmail.com>
2025-03-19 09:43:21 -0500

    base de datos

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import router from './routes/index.js';
+import db from './config/db.js'
 
 const app = express();
 
+//Conectar la base de datos
+db.authenticate()
+    .then (()=>console.log('Bd conectada'))
+    .catch(error => console.log(error))
 
 //Definir puerto
 const port = process.env.PORT || 4000;
